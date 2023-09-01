c4c289bdd822edfbc5359341824fecb9a229635b
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-09-01 22:30:58 +0200

    Code cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+    var darkmode = localStorage.getItem("darkmode");
 
-    var darkmode = localStorage.getItem('darkmode')
-
-    if (darkmode === 'true'){
-        document.body.classList.toggle('dark-mode-variables');
+    if (darkmode === "true") {
+        document.body.classList.toggle("dark-mode-variables");
     }
 
-    const darkModeButton = document.getElementById('darkmode-button');
-    darkModeButton.addEventListener('click', handleDarkModeButtonClick);
-})
-
-
-
+    const darkModeButton = document.getElementById("darkmode-button");
+    darkModeButton.addEventListener("click", handleDarkModeButtonClick);
+});
 
 // Function to handle the dark mode button click event
 function handleDarkModeButtonClick() {
-    const isDarkModeEnabled = localStorage.getItem('darkmode') === 'true';
+    const isDarkModeEnabled = localStorage.getItem("darkmode") === "true";
 
     // Toggle the dark mode CSS class
-    document.body.classList.toggle('dark-mode-variables');
+    document.body.classList.toggle("dark-mode-variables");
 
     // Update the "darkmode" value in localStorage
-    localStorage.setItem('darkmode', !isDarkModeEnabled);
+    localStorage.setItem("darkmode", !isDarkModeEnabled);
 
-    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+    darkMode.querySelector("span:nth-child(1)").classList.toggle("active");
+    darkMode.querySelector("span:nth-child(2)").classList.toggle("active");
 }
-
-
-
-
-
-
-
