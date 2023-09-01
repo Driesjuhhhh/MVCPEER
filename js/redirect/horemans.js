c3dad29af2bebdaee7f2df013aa0f76ad8ab2dee
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-09-01 12:42:46 +0200

    Custom redirect for sponsors

---
+++ js/redirect/horemans.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    openSiteOnLoad();
+})
+
+function openSiteOnLoad() {
+    window.location.href = "https://3bouw.be/?gclid=EAIaIQobChMI-KKilrui5AIVzOR3Ch31iQioEAAYASAAEgLK-_D_BwE";
+}
---
+++ js/redirect/panos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    openSiteOnLoad();
+})
+
+function openSiteOnLoad() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100063551143102";
+}
