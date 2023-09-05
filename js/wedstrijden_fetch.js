c7a0b63d4104419caeb8fb33cf4ccc24eadb425c
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-09-05 11:11:12 +0200

    BUG: table not removing

--- js/wedstrijden_fetch.js
+++ js/wedstrijden_fetch.js
@@ -1,62 +1,32 @@
-const reeksCodeArray = [
-    "P1D",
-    "P3D-B",
-    "LMU17N2R1-E",
-    "LMU15N1R1-A",
-    "LMU15N2R1-C",
-    "LMU13N2R1-E",
-    "LMU13N3R1-B",
-    "LMU13N3R1-C",
-    "LMJU11N3R1-A",
-    "LMJU11N3R1-C",
-    "LMJU11N3R1-D",
-    "LMJU112-2(4.1)R1-B",
-    "BVLPD C",
-    "BVLPD L",
-];
-const reeksNameArray = [
-    "Promo 1 Dames",
-    "Promo 3 Dames",
-    "U17 Meisjes Niveau 2",
-    "U15 Meisjes A",
-    "U15 Meisjes B",
-    "U13 Meisjes A",
-    "U13 Meisjes B",
-    "U13 Meisjes C",
-    "U11 Jongens-Meisjes A",
-    "U11 Jongens-Meisjes B",
-    "U11 Jongens-Meisjes C",
-    "U11 Jongens Meisjes 2v2",
-    "Beker van Limburg Promo 1",
-    "Beker van Limburg Promo 3",
-];
-const tableDiv = document.getElementById("table");
-const select = document.getElementById("ploegenSelect");
-const button = document.getElementById("button");
+const reeksCodeArray = ["P1D","P3D-B","LMU17N2R1-E","LMU15N1R1-A","LMU15N2R1-C","LMU13N2R1-E","LMU13N3R1-B","LMU13N3R1-C","LMJU11N3R1-A","LMJU11N3R1-C","LMJU11N3R1-D","LMJU112-2(4.1)R1-B","BVLPD C","BVLPD L"];
+const reeksNameArray = ["Promo 1 Dames", "Promo 3 Dames", "U17 Meisjes Niveau 2", "U15 Meisjes A", "U15 Meisjes B", "U13 Meisjes A", "U13 Meisjes B", "U13 Meisjes C", "U11 Jongens-Meisjes A", "U11 Jongens-Meisjes B", "U11 Jongens-Meisjes C", "U11 Jongens Meisjes 2v2", "Beker van Limburg Promo 1", "Beker van Limburg Promo 3"];
+const tableDiv = document.getElementById('table');
+const select = document.getElementById('ploegenSelect');
 
-button.addEventListener("click", function () {
-    var seriesIdFromSelect = document.getElementById("ploegenSelect").value;
+select.addEventListener("change", function (){
+    var seriesId = select.value;
     removeTable;
-    getMatchesFunction(seriesIdFromSelect);
+    getMatchesFunction();
 });
 
 // Set the credentials
-const clubNumber = "L-0923";
+const clubNumber = 'L-0923';
 const provinceId = 4;
 const seriesId = select.value;
 
+
 class VolleyAdmin2 {
-    static API_URL = "http://www.volleyadmin2.be/services";
+    static API_URL = 'http://www.volleyadmin2.be/services';
 
     // Possible methods
-    static API_METHOD_STANDINGS = "rangschikking";
-    static API_METHOD_MATCHES = "wedstrijden";
-    static API_METHOD_TEAMS = "series";
+    static API_METHOD_STANDINGS = 'rangschikking';
+    static API_METHOD_MATCHES = 'wedstrijden';
+    static API_METHOD_TEAMS = 'series';
 
     // Possible variables
-    static CLUB_NUMBER = "stamnummer";
-    static PROVINCE_ID = "province_id";
-    static SERIES_ID = "reeks";
+    static CLUB_NUMBER = 'stamnummer';
+    static PROVINCE_ID = 'province_id';
+    static SERIES_ID = 'reeks';
 
     /**
      * Do call
@@ -68,17 +38,15 @@ class VolleyAdmin2 {
      */
     async doCall(method, parameters = {}) {
         // Check if fetch is available
-        if (typeof fetch !== "function") {
-            throw new Error(
-                "This method requires the fetch API, which is not available in this environment."
-            );
+        if (typeof fetch !== 'function') {
+            throw new Error('This method requires the fetch API, which is not available in this environment.');
         }
 
-        parameters.format = "json";
+        parameters.format = 'json';
 
         const queryParams = Object.entries(parameters)
             .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-            .join("&");
+            .join('&');
 
         // Define endPoint
         const endPoint = `${VolleyAdmin2.API_URL}/${method}_xml.php?${queryParams}`;
@@ -86,25 +54,21 @@ class VolleyAdmin2 {
         try {
             // Fetch data
             const response = await fetch(endPoint, {
-                method: "GET",
+                method: 'GET',
                 timeout: 10000,
-            });
+              });
 
             // Handle response errors
             if (!response.ok) {
-                throw new Error(
-                    `Request failed with status: ${response.status}`
-                );
+                throw new Error(`Request failed with status: ${response.status}`);
             }
 
             // Parse response as JSON
             const responseData = await response.json();
-            console.log(responseData);
+            console.log(responseData)
             return responseData;
         } catch (error) {
-            throw new Error(
-                `An error occurred while fetching data: ${error.message}`
-            );
+            throw new Error(`An error occurred while fetching data: ${error.message}`);
         }
     }
 
@@ -124,13 +88,7 @@ class VolleyAdmin2 {
                 continue;
             }
 
-            if (
-                ![
-                    VolleyAdmin2.CLUB_NUMBER,
-                    VolleyAdmin2.PROVINCE_ID,
-                    VolleyAdmin2.SERIES_ID,
-                ].includes(key)
-            ) {
+            if (![VolleyAdmin2.CLUB_NUMBER, VolleyAdmin2.PROVINCE_ID, VolleyAdmin2.SERIES_ID].includes(key)) {
                 throw new Error(`The key "${key}" is invalid.`);
             }
 
@@ -150,14 +108,11 @@ class VolleyAdmin2 {
      * @throws {Error}
      */
     async getMatches(seriesId = null, provinceId = null, clubNumber = null) {
-        return this.doCall(
-            VolleyAdmin2.API_METHOD_MATCHES,
-            this.checkParameters({
-                [VolleyAdmin2.SERIES_ID]: seriesId,
-                [VolleyAdmin2.PROVINCE_ID]: provinceId,
-                [VolleyAdmin2.CLUB_NUMBER]: clubNumber,
-            })
-        );
+        return this.doCall(VolleyAdmin2.API_METHOD_MATCHES, this.checkParameters({
+            [VolleyAdmin2.SERIES_ID]: seriesId,
+            [VolleyAdmin2.PROVINCE_ID]: provinceId,
+            [VolleyAdmin2.CLUB_NUMBER]: clubNumber,
+        }));
     }
 
     /**
@@ -168,12 +123,9 @@ class VolleyAdmin2 {
      * @throws {Error}
      */
     async getSeries(provinceId = null) {
-        return this.doCall(
-            VolleyAdmin2.API_METHOD_TEAMS,
-            this.checkParameters({
-                [VolleyAdmin2.PROVINCE_ID]: provinceId,
-            })
-        );
+        return this.doCall(VolleyAdmin2.API_METHOD_TEAMS, this.checkParameters({
+            [VolleyAdmin2.PROVINCE_ID]: provinceId,
+        }));
     }
 
     /**
@@ -185,13 +137,10 @@ class VolleyAdmin2 {
      * @throws {Error}
      */
     async getStandings(seriesId = null, provinceId = null) {
-        return this.doCall(
-            VolleyAdmin2.API_METHOD_STANDINGS,
-            this.checkParameters({
-                [VolleyAdmin2.SERIES_ID]: seriesId,
-                [VolleyAdmin2.PROVINCE_ID]: provinceId,
-            })
-        );
+        return this.doCall(VolleyAdmin2.API_METHOD_STANDINGS, this.checkParameters({
+            [VolleyAdmin2.SERIES_ID]: seriesId,
+            [VolleyAdmin2.PROVINCE_ID]: provinceId,
+        }));
     }
 }
 
@@ -199,68 +148,67 @@ class VolleyAdmin2 {
 const volleyAdmin = new VolleyAdmin2();
 
 // Example: Get matches
-function getMatchesFunction(seriesId) {
-    volleyAdmin
-        .getMatches(seriesId, provinceId, clubNumber)
-        .then((matches) => {
-            if (tableDiv) {
-                const table = document.createElement("table");
-                table.innerHTML = "<tr><th>Ploeg</th><th>Datum</th></tr>";
+function getMatchesFunction(seriesId){
+    volleyAdmin.getMatches(seriesId, provinceId, clubNumber)
+    .then((matches) => {
+        if (tableDiv) {
+            const table = document.createElement('table');
+            table.innerHTML = '<tr><th>Ploeg</th><th>Datum</th></tr>';
 
-                matches.forEach((element) => {
-                    const ploegnummer = element.Reeks;
-                    for (let i = 0; i < reeksCodeArray.length; i++) {
-                        if (reeksCodeArray[i] === ploegnummer) {
-                            var ploeg = reeksNameArray[i];
-                        }
+            matches.forEach(element => {
+                const ploegnummer = element.Reeks;
+                for (let i = 0; i < reeksCodeArray.length; i++) {
+                    if (reeksCodeArray[i] === ploegnummer){
+                        var ploeg = reeksNameArray[i]
                     }
-                    const wedstrijdnummer = element.Wedstrijdnr;
+                  }
+                const wedstrijdnummer = element.Wedstrijdnr;
+                
+                const row = document.createElement('tr');
+                row.innerHTML = `<td>${ploeg}</td><td>${element.t}</td>`;
+                
+                table.appendChild(row);
+            });
 
-                    const row = document.createElement("tr");
-                    row.innerHTML = `<td>${ploeg}</td><td>${element.t}</td>`;
-
-                    table.appendChild(row);
-                });
-
-                tableDiv.appendChild(table);
-            } else {
-                console.error("Error");
-            }
-        })
-        .catch((error) => {
-            table.innerHTML = `<p>Error<p>`;
-            console.error("Error:", error.message);
-        });
-}
+            tableDiv.appendChild(table);
+        } else {
+            console.error('Error');
+        }
+    })
+    .catch((error) => {
+        table.innerHTML = `<p>Error<p>`;
+        console.error('Error:', error.message);
+    });
+};
 
 // Example: Get series
-function getSeriesFunction() {
-    volleyAdmin
-        .getSeries(provinceId)
-        .then((series) => {
-            // console.log('Series:', series);
-        })
-        .catch((error) => {
-            // table.innerHTML = `<p>Error<p>`;
-            // console.error('Error:', error.message);
-        });
-}
+function getSeriesFunction(){
+    volleyAdmin.getSeries(provinceId)
+    .then((series) => {
+        // console.log('Series:', series);
+    })
+    .catch((error) => {
+        // table.innerHTML = `<p>Error<p>`;
+        // console.error('Error:', error.message);
+    });
+};
 
-function getStandingsFunction() {
-    // Example: Get standings
-    volleyAdmin
-        .getStandings(seriesId, provinceId)
-        .then((standings) => {
-            // console.log('Standings:', standings);
-        })
-        .catch((error) => {
-            // table.innerHTML = `<p>Error<p>`;
-            // console.log(error)
-        });
-}
+
+function getStandingsFunction(){
+// Example: Get standings
+volleyAdmin.getStandings(seriesId, provinceId)
+    .then((standings) => {
+        // console.log('Standings:', standings);
+    })
+    .catch((error) => {
+        // table.innerHTML = `<p>Error<p>`;
+        // console.log(error)
+    });
+};
 
 function removeTable() {
     if (table.parentNode) {
         table.parentNode.removeChild(table);
     }
 }
+
