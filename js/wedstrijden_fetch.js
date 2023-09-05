dab4c91a8796093294cdf6dbe0f700ffe4a8e10e
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-09-05 11:47:41 +0200

    quick fix

--- js/wedstrijden_fetch.js
+++ js/wedstrijden_fetch.js
@@ -6,7 +6,7 @@ const table = document.createElement('table');
 const button = document.getElementById('button');
 
 button.addEventListener("click", function (){
-    tableContainer.innerHTML = '';
+    tableDiv.innerHTML = '';
     getMatchesFunction();
     
    
