4b4a2449489c4bfe3dbf3807416717fdc2133a1f
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-09-05 11:41:45 +0200

    test

--- js/wedstrijden_fetch.js
+++ js/wedstrijden_fetch.js
@@ -6,7 +6,7 @@ const table = document.createElement('table');
 const button = document.getElementById('button');
 
 button.addEventListener("click", function (){
-    removeTable();
+    document.removeChild('table');
     getMatchesFunction();
     
    
