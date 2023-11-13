b4fceadc695a0aaabb28cd1112e81395fe8a8453
DriesBielenPXL <dries.bielen10@gmail.com>
2023-11-13 21:24:26 +0100

    Total rewrite
    Implemented boodstrap and did a complete rewrite using bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-'use strict'
-
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Leuk dat je de code van deze website bekijkt!");
     console.log("Deze code is geschreven door Dries Bielen");
     console.log("https://www.driesbielen.be");
     console.log("Have a good time in inspector land");
     setCurrentYear();
+
     var darkmode = localStorage.getItem("darkmode");
 
     if (darkmode === "true") {
@@ -13,12 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const darkModeButton = document.getElementById("darkmode-button");
-    const mobileDarkModeButton = document.getElementById("mobile-darkmode-button");
-    mobileDarkModeButton.addEventListener("click", handleDarkModeButtonClick);
     darkModeButton.addEventListener("click", handleDarkModeButtonClick);
 });
 
-// Function to handle the dark mode button click event
+function setCurrentYear() {
+    var currentYear = new Date().getFullYear();
+    var yearSpan = document.getElementById("year");
+    if (yearSpan) {
+        yearSpan.textContent = "© " + currentYear + " Mvc Peer";
+    }
+}
+
 function handleDarkModeButtonClick() {
     const isDarkModeEnabled = localStorage.getItem("darkmode") === "true";
 
@@ -31,21 +35,3 @@ function handleDarkModeButtonClick() {
     darkMode.querySelector("span:nth-child(1)").classList.toggle("active");
     darkMode.querySelector("span:nth-child(2)").classList.toggle("active");
 }
-
-
-function setCurrentYear() {
-    var currentYear = new Date().getFullYear();
-    var yearSpan = document.getElementById("year");
-    if (yearSpan) {
-        yearSpan.textContent = currentYear;
-    }
-}
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  
