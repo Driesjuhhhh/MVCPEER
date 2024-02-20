70e22ca2b9455be1efd484759e04fa7a67c4b584
Dries Bielen <dries.bielen10@gmail.com>
2024-02-20 16:37:28 +0100

    Added next match to index

--- js/index.js
+++ js/index.js
@@ -17,4 +17,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Call the updateText function every 2 seconds
     setInterval(updateText, 2000);
+
+    const toastLiveExample = document.getElementById('liveToast');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastBootstrap.show();
 });
+
+
+function fetchNextMatch() {
+    const url = "https://www.volleyadmin2.be/services/wedstrijden_xml.php?province_id=4&stamnummer=L-0923&format=json";
+
+    // Selecteer de lijstitems waar je de informatie wilt weergeven
+    const wedstrijdsporthal = document.querySelector(".volgendewedstrijd-sporthal");
+    const wedstrijddatum = document.querySelector(".volgendewedstrijd-datum");
+    const wedstrijduur = document.querySelector(".volgendewedstrijd-tijd");
+
+    // Fetch data using Fetch API
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            // Filter de wedstrijden die nog moeten plaatsvinden
+            const wedstrijden = eerstvolgendeWedstrijden(data)
+
+            // Neem de gegevens van de eerstvolgende wedstrijd
+            const eerstvolgendeWedstrijd = wedstrijden[0];
+
+            // Update de innerHTML van de lijstitems
+            wedstrijdsporthal.innerHTML = eerstvolgendeWedstrijd
+                ? `<span>Locatie: ${eerstvolgendeWedstrijd.SporthalNaam}</span>`
+                : "<span>Geen komende wedstrijden gevonden.</span>";
+
+            wedstrijddatum.innerHTML = `<span>Datum: ${eerstvolgendeWedstrijd.t}</span>`
+
+            wedstrijduur.innerHTML = `<span>Uur: ${eerstvolgendeWedstrijd.Aanvangsuur.substr(
+                0,
+                5
+            )}</span>`
+        })
+        .catch(error => console.error("Fout bij het ophalen van gegevens:", error));
+}
+
+function eerstvolgendeWedstrijden(data) {
+    const futureDates = []
+
+    for (let i = 0; i < data.length; i++) {
+        const wedstrijd = data[i]
+        //console.log(time)
+        const date = wedstrijd.t.split("/").reverse().join("-");
+        //console.log(date)
+        const alreadyHappened = new Date(date) < new Date();
+        let location;
+        if ("Sporthal de Deuster, Peer" == wedstrijd.SporthalNaam){
+            location = true;
+        } else {
+            location = false;
+        }
+
+        if (!alreadyHappened && location) {
+            futureDates.push(wedstrijd)
+        }
+    }
+
+    return futureDates
+}
