68c6326b01a845dcb20f26ac09f2796314ed4e8b
Dries Bielen <dries.bielen10@gmail.com>
2024-02-20 16:00:07 +0100

    added code for promo3

--- js/ploegen.js
+++ js/ploegen.js
@@ -1,4 +1,9 @@
-const url = "https://www.volleyadmin2.be/services/wedstrijden_xml.php?province_id=4&stamnummer=L-0923&seriesid=p1d&format=json";
+fetchpromo1();
+fetchpromo3();
+
+
+function fetchpromo1() {
+    const url = "https://www.volleyadmin2.be/services/wedstrijden_xml.php?province_id=4&stamnummer=L-0923&seriesid=p1d&format=json";
 
 // Selecteer de lijstitems waar je de informatie wilt weergeven
 const wedstrijdsporthal = document.querySelector(".volgendewedstrijdpromo-1-sporthal");
@@ -26,6 +31,38 @@ fetch(url)
     wedstrijduur.innerHTML = `<span>Uur: ${eerstvolgendeWedstrijd.Aanvangsuur}</span>`
   })
   .catch(error => console.error("Fout bij het ophalen van gegevens:", error));
+}
+
+function fetchpromo3() {
+    const url = "https://www.volleyadmin2.be/services/wedstrijden_xml.php?province_id=4&stamnummer=L-0923&seriesid=p3d-b&format=json";
+
+// Selecteer de lijstitems waar je de informatie wilt weergeven
+const wedstrijdsporthal = document.querySelector(".volgendewedstrijdpromo-3-sporthal");
+const wedstrijddatum = document.querySelector(".volgendewedstrijdpromo-3-datum");
+const wedstrijduur = document.querySelector(".volgendewedstrijdpromo-3-tijd");
+
+// Fetch data using Fetch API
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+
+    // Filter de wedstrijden die nog moeten plaatsvinden
+    const wedstrijden = eerstvolgendeWedstrijden(data)
+
+    // Neem de gegevens van de eerstvolgende wedstrijd
+    const eerstvolgendeWedstrijd = wedstrijden[0];
+
+    // Update de innerHTML van de lijstitems
+    wedstrijdsporthal.innerHTML = eerstvolgendeWedstrijd
+      ? `<span>Locatie: ${eerstvolgendeWedstrijd.SporthalNaam}</span>`
+      : "<span>Geen komende wedstrijden gevonden.</span>";
+
+    wedstrijddatum.innerHTML = `<span>Datum: ${eerstvolgendeWedstrijd.t}</span>`
+
+    wedstrijduur.innerHTML = `<span>Uur: ${eerstvolgendeWedstrijd.Aanvangsuur}</span>`
+  })
+  .catch(error => console.error("Fout bij het ophalen van gegevens:", error));
+}
 
 function eerstvolgendeWedstrijden(data) {
     const futureDates = []
